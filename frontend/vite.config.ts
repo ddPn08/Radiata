83f46329e0492ed392c617b0f78d524f54df75d9
unknown <git@p1at.dev>
2023-02-06 17:29:41 +0900

    migrating to react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,7 @@
-import path from 'path'
-import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/app',
-    build: { target: 'esnext' },
-    resolve: {
-        alias: {
-            '~': path.resolve(__dirname, 'src'),
-            'internal:api': path.resolve(__dirname, 'modules', 'api'),
-        },
-    },
-    plugins: [
-        Icons({
-            compiler: 'solid',
-        }),
-        solidPlugin(),
-    ],
+  plugins: [react()],
 })
