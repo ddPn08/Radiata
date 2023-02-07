e30afda23c769f527b1b977d1a11cc329986c62a
unknown <git@p1at.dev>
2023-02-07 17:33:29 +0900

    chore: add dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/app',
+  // build: { target: 'esnext' },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      'internal:api': path.resolve(__dirname, 'modules', 'api'),
+    },
+  },
   plugins: [react()],
 })
