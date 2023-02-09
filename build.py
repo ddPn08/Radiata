5f5eabddf6adaf37728f566d3221a705e8bb5688
ddPn08 <pyn.g.1125@gmail.com>
2023-02-09 21:01:41 +0900

    small fix

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ __dirname__ = os.path.dirname(__file__)
 def build_frontend():
     frontend_dir = os.path.join(__dirname__, "frontend")
     out_dir = os.path.join(__dirname__, "dist")
-    if os.path.exists(shutil.rmtree(out_dir)):
+    if os.path.exists(out_dir):
         shutil.rmtree(out_dir)
     subprocess.run(
         ["pnpm", "i"],
