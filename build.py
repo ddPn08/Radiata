09544eb29e48fcd7a2ff5432dc88e1ad0ba67455
ddPn08 <pyn.g.1125@gmail.com>
2023-02-11 15:15:03 +0900

    fix build script

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 import os
 import shutil
 import subprocess
+import platform
 
 __dirname__ = os.path.dirname(__file__)
+pnpm = "pnpm.cmd" if platform.system() == "Windows" else "pnpm"
 
 
 def build_frontend():
@@ -11,11 +13,11 @@ def build_frontend():
     if os.path.exists(out_dir):
         shutil.rmtree(out_dir)
     subprocess.run(
-        ["pnpm", "i"],
+        [pnpm, "i"],
         cwd=frontend_dir,
     )
     subprocess.run(
-        ["pnpm", "build", "--out-dir", out_dir],
+        [pnpm, "build", "--out-dir", out_dir],
         cwd=frontend_dir,
     )
 
