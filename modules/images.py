26a8d0ac73c7b1bc8d932abb64f86f9d06c9378c
ふぁ <yuki@yuki0311.com>
2023-02-15 14:23:22 +0900

    fix png info
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- modules/images.py
+++ modules/images.py
@@ -20,22 +20,22 @@ def save_image(img: Image.Image, info: Dict):
     metadata.add_text("parameters", json.dumps(parameters))
     dir = config.get(f"images/{get_category(info)}/save_dir")
     basename: str = config.get(f"images/{get_category(info)}/save_name")
-    filename = (
-        basename.replace("{seed}", f'{info["seed"]}')
-        .replace(
-            "{index}", f"{len(os.listdir(dir)) + 1}" if os.path.exists(dir) else "0"
-        )
-        .replace("{prompt}", info["prompt"][:20].replace(" ", "_"))
-        .replace("{date}", datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    filename = basename.format(
+        seed=info["seed"],
+        index=len(os.listdir(dir)) + 1 if os.path.exists(dir) else 0,
+        prompt=info["prompt"][:20].replace(" ", "_"),
+        date=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
     )
     os.makedirs(dir, exist_ok=True)
     filepath = os.path.join(dir, filename)
-    img.save(filepath, metadata=metadata)
+    img.save(filepath, pnginfo=metadata)
     return os.path.basename(filepath)
 
 
-def get_image(category: str, filename: str):
+def get_image_filepath(category: str, filename: str):
     dir = config.get(f"images/{category}/save_dir")
-    filepath = os.path.join(dir, filename)
-    img = Image.open(filepath)
-    return img
+    return os.path.join(dir, filename)
+
+
+def get_image(category: str, filename: str):
+    return Image.open(get_image_filepath(category,filename))
