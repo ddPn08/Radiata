c70a5cd401989d42218a1e9f6164d1f0efc985f6
ふぁ <yuki@yuki0311.com>
2023-02-16 23:38:33 +0900

    update json decode
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- frontend/src/utils/stream.ts
+++ frontend/src/utils/stream.ts
@@ -1,16 +1,15 @@
 export const streamGenerator = async function* (body: ReadableStream<Uint8Array>) {
     const reader = body?.getReader()
-    const placeholder = '{placeholder}'
     if (!reader) return
     while (true) {
         const res = await reader.read()
         if (res.done) return
         try {
-            const text = new TextDecoder()
-                .decode(res.value)
-                .replaceAll('}{', `}${placeholder}{`)
-                .split(placeholder)
-            yield JSON.parse(text[text.length - 1])
-        } catch (_) {}
+            console.log(new TextDecoder().decode(res.value))
+            const text = new TextDecoder().decode(res.value).split('\n')
+            yield JSON.parse(text[text.length - 2])
+        } catch (e) {
+            console.error(e)
+        }
     }
 }
