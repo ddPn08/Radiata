af13cacb21d3d89edbe5a9fcabfa950213a5900f
ddPn08 <pyn.g.1125@gmail.com>
2023-05-01 23:36:30 +0900

    update docsearch data

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -129,9 +129,9 @@ export default defineConfig({
             },
         ],
         algolia: {
-            appId: 'H0ENKDAOQE',
-            indexName: 'lsmith',
-            apiKey: '292dd349b89a56475ea02a16d4dac69b',
+            appId: 'FOBPD1HX85',
+            indexName: 'radiata',
+            apiKey: 'ec8f44d4b2911833c7ed37d2bdbb21c9',
         },
     },
 })
