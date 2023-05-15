61226b711d8c073a6bfc78235219187960afea54
Isotr0py <2037008807@qq.com>
2023-05-15 19:11:08 +0800

    comment unavailable schedulers

--- lib/diffusers/scheduler.py
+++ lib/diffusers/scheduler.py
@@ -1,6 +1,7 @@
 import diffusers
 
 # TODO: add rest Auto111 samplers
+# Commented schedulers are still unavailable in diffusers 0.16.1
 SCHEDULERS = {
     "unipc": diffusers.schedulers.UniPCMultistepScheduler,
     "euler_a": diffusers.schedulers.EulerAncestralDiscreteScheduler,
@@ -13,12 +14,12 @@ SCHEDULERS = {
     "dpm++_2s": diffusers.schedulers.DPMSolverSinglestepScheduler,
     "dpm++_2m": diffusers.schedulers.DPMSolverMultistepScheduler,
     "dpm++_2m_karras": diffusers.schedulers.DPMSolverMultistepScheduler,
-    "dpm++_sde": diffusers.schedulers.DPMSolverSDEScheduler,
-    "dpm++_sde_karras": diffusers.schedulers.DPMSolverSDEScheduler,
+    # "dpm++_sde": diffusers.schedulers.DPMSolverSDEScheduler,
+    # "dpm++_sde_karras": diffusers.schedulers.DPMSolverSDEScheduler,
     "heun": diffusers.schedulers.HeunDiscreteScheduler,
     "heun_karras": diffusers.schedulers.HeunDiscreteScheduler,
     "lms": diffusers.schedulers.LMSDiscreteScheduler,
-    # "lms_karras": diffusers.schedulers.LMSDiscreteScheduler, # still unreleased in diffusers 0.16.0
+    # "lms_karras": diffusers.schedulers.LMSDiscreteScheduler,
     "pndm": diffusers.schedulers.PNDMScheduler,
 }
 
